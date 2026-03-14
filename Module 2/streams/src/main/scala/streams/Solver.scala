a3eb535b3a15b3fbee2aab66d9b33f6dd5142ccf
Christensen, Jacob (US) <Jacob.Christensen01@Teledyne.com>
2026-03-14 16:59:55 -0700

    Add module 2 assignment

---
+++ Module 2/streams/src/main/scala/streams/Solver.scala
@@ -0,0 +1,108 @@
+package streams
+
+/**
+ * This component implements the solver for the Bloxorz game
+ */
+trait Solver extends GameDef:
+
+  /**
+   * Returns `true` if the block `b` is at the final position
+   */
+  def done(b: Block): Boolean =
+    b.isStanding && (b.b1.row == goal.row && b.b1.col == goal.col)
+
+  /**
+   * This function takes two arguments: the current block `b` and
+   * a list of moves `history` that was required to reach the
+   * position of `b`.
+   *
+   * The `head` element of the `history` list is the latest move
+   * that was executed, i.e. the last move that was performed for
+   * the block to end up at position `b`.
+   *
+   * The function returns a lazy list of pairs: the first element of
+   * the each pair is a neighboring block, and the second element
+   * is the augmented history of moves required to reach this block.
+   *
+   * It should only return valid neighbors, i.e. block positions
+   * that are inside the terrain.
+   */
+  def neighborsWithHistory(b: Block, history: List[Move]): LazyList[(Block, List[Move])] =
+    for
+      (newBlock, move) <- b.legalNeighbors.to(LazyList)
+    yield
+      (newBlock, move :: history)
+
+  /**
+   * This function returns the list of neighbors without the block
+   * positions that have already been explored. We will use it to
+   * make sure that we don't explore circular paths.
+   */
+  def newNeighborsOnly(neighbors: LazyList[(Block, List[Move])],
+                       explored: Set[Block]): LazyList[(Block, List[Move])] =
+    for 
+      (block, hist) <- neighbors
+      if !explored.contains(block)
+    yield (block, hist)
+
+  /**
+   * The function `from` returns the lazy list of all possible paths
+   * that can be followed, starting at the `head` of the `initial`
+   * lazy list.
+   *
+   * The blocks in the lazy list `initial` are sorted by ascending path
+   * length: the block positions with the shortest paths (length of
+   * move list) are at the head of the lazy list.
+   *
+   * The parameter `explored` is a set of block positions that have
+   * been visited before, on the path to any of the blocks in the
+   * lazy list `initial`. When search reaches a block that has already
+   * been explored before, that position should not be included a
+   * second time to avoid cycles.
+   *
+   * The resulting lazy list should be sorted by ascending path length,
+   * i.e. the block positions that can be reached with the fewest
+   * amount of moves should appear first in the lazy list.
+   *
+   * Note: the solution should not look at or compare the lengths
+   * of different paths - the implementation should naturally
+   * construct the correctly sorted lazy list.
+   */
+  def from(initial: LazyList[(Block, List[Move])],
+           explored: Set[Block]): LazyList[(Block, List[Move])] =
+    val frontier =
+      for
+        (block, moves) <- initial
+        (newBlock, newMoves) <- newNeighborsOnly(neighborsWithHistory(block, moves), explored)
+      yield (newBlock, newMoves)
+    initial #::: from(frontier, explored ++ frontier.map((b, m) => b))
+
+  /**
+   * The lazy list of all paths that begin at the starting block.
+   */
+  lazy val pathsFromStart: LazyList[(Block, List[Move])] =
+    val startBlock = Block(startPos, startPos)
+    from(LazyList((startBlock, Nil)), Set(startBlock))
+
+  /**
+   * Returns a lazy list of all possible pairs of the goal block along
+   * with the history how it was reached.
+   */
+  lazy val pathsToGoal: LazyList[(Block, List[Move])] =
+    for 
+      (block, moves) <- pathsFromStart
+      if done(block)
+    yield (block, moves)
+
+  /**
+   * The (or one of the) shortest sequence(s) of moves to reach the
+   * goal. If the goal cannot be reached, the empty list is returned.
+   *
+   * Note: the `head` element of the returned list should represent
+   * the first move that the player should perform from the starting
+   * position.
+   */
+  lazy val solution: List[Move] =
+    if pathsToGoal.isEmpty then Nil
+    else
+      pathsToGoal.head._2.reverse
